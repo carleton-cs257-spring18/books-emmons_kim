866b4b6ba4821c2c9eee39cbb74229a72da2f2a6
Alli <student@eduroam-171-20.dyn.carleton.edu>
2019-04-06 13:33:45 -0500

    Updated main to catch argument errors

--- books/books1.py
+++ books/books1.py
@@ -1,4 +1,4 @@
-import sys
+import sys, csv
 
 def sort_books(file, how):
     print("sort_books")
@@ -10,22 +10,44 @@ def sort_reverse(list):
     #use list and reverse, print
     print("reverse print")
 
-def main():
-    file_object = open(sys.argv[1], "r")
-    action = sys.argv[2]
-    try:
-        direction = sys.argv[3]
-    except:
-        direction = ""
-
-    if action == "books": #sort by titles
-        sort_books(file_object, direction)
-
-    elif action == "authors": #sort by authors
-        sort_authors(file_object, direction)
-        
-    else: #arguments not fitting
+def main():    
+    if len(sys.argv) < 3 or len(sys.argv) > 4:
         print('Usage: blah blah blah', file=sys.stderr)
+        
+    else:
+        try:
+            file_object = open(sys.argv[1], "r")
+        except:
+            print('Usage: blah blah blah', file=sys.stderr)
+            return
+
+        try:
+            action = sys.argv[2]
+        except:
+            print('Usage: blah blah blah', file=sys.stderr)
+            return
+        
+        try:
+            direction = sys.argv[3]
+            if direction == "reverse":
+                direction = False #direction is a Boolean
+            elif direction == "forward":
+                direction = True
+            else:
+                print('Usage: blah blah blah', file=sys.stderr)
+                return
+            
+        except:
+            direction = True
+
+        if action == "books": #sort by titles
+            sort_books(file_object, direction)
+            
+        elif action == "authors": #sort by authors
+            sort_authors(file_object, direction)
+            
+        else: #arguments not fitting
+            print('Usage: blah blah blah', file=sys.stderr)
     
 main()
 
