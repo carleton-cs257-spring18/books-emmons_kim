06c265de63b4d43c65f846b9948c3efefa753aca
Alli <student@eduroam-171-20.dyn.carleton.edu>
2019-04-07 15:31:56 -0500

    Added comments

--- books/books1.py
+++ books/books1.py
@@ -1,27 +1,34 @@
 import sys, csv
 import operator
 
+
+#This function's parameters are a cvs filename that contains book titles and authors,
+#and a Boolean that checks whether the list should be alphabetical in ascending or
+#descending order. Its output is a printed list of book titles, either in alphabetical
+#or reverse alphabetical order.
 def sort_books(file, how):
     with open(file, newline = "") as csvfile:
         list = csv.reader(csvfile)
+        #Sorts the items in the file by book title, column 1.
         sorted_books = sorted(list, key=operator.itemgetter(0))
         if how:
             for book in sorted_books: 
                 print(book[0], sep = "\n")
         else:
+            #Prints the list in reverse order.
             sorted_books.reverse()
             for book in sorted_books: 
                 print(book[0], sep = "\n")
 
-#This function takes in a cvs filename that contains book titles and 
-# as well as taking in a boolean. Its output is a printed list of authors 
-# in alphabetical order or reverse alphabetical order depending on if the 
-# boolean is true or false respectively. 
+#This function's parameters are a cvs filename that contains book titles and authors,
+#and a Boolean that checks whether the list should be alphabetical in ascending or
+#descending order. Its output is a printed list of authors, either in alphabetical
+#or reverse alphabetical order.
 def sort_authors(filename, boolean):
     names = []
     with open(filename) as csvfile:
         books = csv.reader(csvfile, delimiter=",")
-        #This loop goes through the csv file and extracts the quthors name 
+        #Loops through the file and extracts the authors' names 
         #from each row and adds them to a list. 
         for row in books:
             name = row[2].split(" (")
@@ -29,25 +36,32 @@ def sort_authors(filename, boolean):
             names.append(name)
         #Sorts the books in alphabetical order by last name. 
         names = sorted(names, key = lambda x: x.split(" ")[-1])
-        #Prints the list in reverse. 
+        #Prints the list in reverse order. 
         if boolean == False:
             names_rev = []
             for y in names:
                 names_rev.insert(0, y)
             for j in names_rev:
                 print(j)
-        #Prints the list forward. 
         else:
             for x in names:
                 print(x)    
 
-def main():    
+def main():
+    #Catches command line argument error:
+    #should include all 3 or 4 requirements.
     if len(sys.argv) < 3 or len(sys.argv) > 4:
         print('Usage: blah blah blah', file=sys.stderr)
         
     else:
+        file_object = sys.argv[1]
+
         try:
-            file_object = sys.argv[1]
+            action = sys.argv[2]
+            #Makes sure csv file being called is valid.
+            if FileNotFoundError:
+                print('Usage: blah blah blah', file=sys.stderr)
+                return
         except:
             print('Usage: blah blah blah', file=sys.stderr)
             return
@@ -59,27 +73,31 @@ def main():
             return
         
         try:
+            #Using command line arguments, checks if list to be printed should be in
+            #alphabetical or reverse alphabetical order.
             direction = sys.argv[3]
             if direction == "reverse":
-                direction = False #direction is a Boolean
+                direction = False
             elif direction == "forward":
                 direction = True
             else:
                 print('Usage: blah blah blah', file=sys.stderr)
                 return
-            
         except:
+            #The direction argument is not mandatory. Consider absent argument as
+            #"forward".
             direction = True
 
-        if action == "books": #sort by titles
+        if action == "books": #Sorts by title.
             sort_books(file_object, direction)
             
-        elif action == "authors": #sort by authors
+        elif action == "authors": #Sorts by author.
             sort_authors(file_object, direction)
             
-        else: #arguments not fitting
+        else: #Other argument issues?
             print('Usage: blah blah blah', file=sys.stderr)
     
-main()
+if __name__ == '__main__':
+    main()
 
 
