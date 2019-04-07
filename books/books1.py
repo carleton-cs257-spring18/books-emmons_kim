b05d6b892150a055d12c18e9592678976c913168
Alli <student@eduroam-171-20.dyn.carleton.edu>
2019-04-07 15:03:49 -0500

    Added sorting by author

--- books/books1.py
+++ books/books1.py
@@ -8,18 +8,36 @@ def sort_books(file, how):
         if how:
             print(*sorted_books, sep = "\n")
         else:
-            sort_reverse(sorted_books)
+            sorted_books.reverse()
+            print(*list, sep = "\n")
 
-def sort_authors(file, how):
-    sorted_authors = ["sorted", "authors"]
-    if how:
-        print(*sorted_authors, sep = "\n")
-    else:
-        sort_reverse(sorted_authors)
-    
-def sort_reverse(list):
-    list.reverse()
-    print(*list, sep = "\n")
+#This function takes in a cvs filename that contains book titles and 
+# as well as taking in a boolean. Its output is a printed list of authors 
+# in alphabetical order or reverse alphabetical order depending on if the 
+# boolean is true or false respectively. 
+def sort_authors(filename, boolean):
+    names = []
+    with open(filename) as csvfile:
+        books = csv.reader(csvfile, delimiter=",")
+        #This loop goes through the csv file and extracts the quthors name 
+        #from each row and adds them to a list. 
+        for row in books:
+            name = row[2].split(" (")
+            name = name[0]
+            names.append(name)
+        #Sorts the books in alphabetical order by last name. 
+        names = sorted(names, key = lambda x: x.split(" ")[-1])
+        #Prints the list in reverse. 
+        if boolean == False:
+            names_rev = []
+            for y in names:
+                names_rev.insert(0, y)
+            for j in names_rev:
+                print(j)
+        #Prints the list forward. 
+        else:
+            for x in names:
+                print(x)    
 
 def main():    
     if len(sys.argv) < 3 or len(sys.argv) > 4:
