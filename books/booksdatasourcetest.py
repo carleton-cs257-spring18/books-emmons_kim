c0a05f429ee9001b8349af5972897a3ed0ab87ca
Alli <student@eduroam-171-20.dyn.carleton.edu>
2019-04-12 11:21:17 -0500

    Added more tests for authors

--- books/booksdatasourcetest.py
+++ books/booksdatasourcetest.py
@@ -22,34 +22,35 @@ class BooksDataSourceTest(unittest.TestCase):
 
     #Tests for 2nd function: books: 3
     
-    def test_small_author_id(self):
+    def test_small_author_id_books(self):
         self.assertRaises(ValueError, self.source_checker.books, author_id=-4)
 
-    def test_big_author_id(self):
+    def test_big_author_id_books(self):
         self.assertRaises(ValueError, self.source_checker.books, author_id=312)
 
-    def test_books_match(self):
+    def test_books_match_books(self):
         self.assertEqual(self.source_checker.books(author_id=13), ["Moby Dick", "Omoo"])
 
     #Tests for 3rd function: author: 3
 
-    def test_small_book_id(self):
-        self.assertRaises(ValueError, self.source_checker.author, book_id=-6)
+    def test_small_author_id(self):
+        self.assertRaises(ValueError, self.source_checker.author, author_id=-6)
 
-    def test_big_book_id(self):
-        self.assertRaises(ValueError, self.source_checker.author, book_id=600)
+    def test_big_author_id(self):
+        self.assertRaises(ValueError, self.source_checker.author, author_id=600)
     
     def test_author_match(self):
-        self.assertEqual(self.source_checker.author(book_id=9), ["Márquez", "Gabriel Garcia"])
+        self.assertEqual(self.source_checker.author(author_id=9), ["Márquez", "Gabriel Garcia"])
 
     #Tests for 4th function: authors: 14
 
     #book_id: 5
 
-    '''
     def test_small_book_id_authors(self):
-    def test_big_book_id_authors(self):  
-    '''
+        self.assertRaises(ValueError, self.source_checker.book, -786)
+        
+    def test_big_book_id_authors(self):
+        self.assertRaises(ValueError, self.source_checker.book, 67)
     
     def test_authors_default_match(self):
         self.assertEqual(self.source_checker.authors(book_id=23), ["Wodehouse", "Pelham Grenville"])
@@ -76,18 +77,16 @@ class BooksDataSourceTest(unittest.TestCase):
     def test_start_year_default_match(self):
         self.assertEqual(self.source_checker.authors(start_year=1960), [["Alderman", "Naomi"], ["Bujold", "Lois McMaster"], ["Carré", "John Le"], ["Christie", "Agatha"], ["DuMaurier", "Daphne"], ["Gaiman", "Neil"], ["Jemisen", "N.K."], ["Lewis", "Sinclair"], ["Murakami", "Haruki"], ["Márquez", "Gabriel García"], ["Morrison", "Toni"], ["Pratchett", "Terry"], ["Rushdie", "Salman"], ["Willis", "Connie"], ["Wodehouse", "Pelham Grenvile"]])
 
-    '''
-    def test_start_year_bith_year_match(self):
+    def test_start_year_birth_year_match(self):
+        self.assertEqual(self.source_checker.authors(start_year = 2000, sort_by = "birth_year"),[["Lewis", "Sinclair"], ["Márquez", "Gabriel Garcia"], ["Carré", "John Le"], ["Morrison", "Toni"], ["Willis", "Connie"], ["Rushdie", "Salman"], ["Bujold", "Lois McMaster"], ["Murakami", "Haruki"], ["Prachett", "Terry"], ["Gainman", "Neil"],  ["Bujold", "Lois McMaster"], ["Jemisen", "N.K."], ["Alderman", "Naomi"]])
+    
     def test_start_year_other_value_match(self):
-    '''
+        self.assertEqual(self.source_checker.authors(start_year = 2019, sort_by = "value"), [["Alderman", "Naomi"], ["Bujold", "Lois McMaster"], ["Carré", "John Le"], ["Gaiman", "Neil"], ["Jemisen", "N.K."], ["Lewis", "Sinclair"], ["Murakami", "Haruki"],  ["Morrison", "Toni"],  ["Rushdie", "Salman"], ["Willis", "Connie"]]) 
 
     #end_year: 3
+
+    def test_end_year_default_match(self):
+        self.assertEqual(self.source_checker.authors(end_year=1861), [["Austen", "Jane"], ["Brontë", "Ann"], ["Brontë", "Charlotte"], ["Bujold", "Lois McMaster"], ["Christie", "Agatha"], ["Dickens", "Charles"], ["Eliot", "George"], ["Jerome", "Jerome K."], ["Melville", "Herman"], ["Rushdie", "Salman"]])
     
 if __name__ == '__main__':
     unittest.main()
-
-#Test out author and book ids again?
-#Test for unmatch also?
-#What do you mean by other value?
-#What about repeated names?
-                                    
