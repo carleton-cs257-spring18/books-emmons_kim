fa1379f9c18c44e39363830cfd4d49f978ac7f05
Alli <student@eduroam-171-20.dyn.carleton.edu>
2019-04-14 14:34:10 -0500

    Error from openfile

--- books/booksdatasource.py
+++ books/booksdatasource.py
@@ -11,75 +11,17 @@
 import csv, operator
 
 class BooksDataSource:
-    '''
-    A BooksDataSource object provides access to data about books and authors.
-    The particular form in which the books and authors are stored will
-    depend on the context (i.e. on the particular assignment you're
-    working on at the time).
-
-    Most of this class's methods return Python lists, dictionaries, or
-    strings representing books, authors, and related information.
-
-    An author is represented as a dictionary with the keys
-    'id', 'last_name', 'first_name', 'birth_year', and 'death_year'.
-    For example, Jane Austen would be represented like this
-    (assuming her database-internal ID number is 72):
-
-        {'id': 72, 'last_name': 'Austen', 'first_name': 'Jane',
-         'birth_year': 1775, 'death_year': 1817}
-
-    For a living author, the death_year is represented in the author's
-    Python dictionary as None.
-
-        {'id': 77, 'last_name': 'Murakami', 'first_name': 'Haruki',
-         'birth_year': 1949, 'death_year': None}
-
-    Note that this is a simple-minded representation of a person in
-    several ways. For example, how do you represent the birth year
-    of Sophocles? What is the last name of Gabriel García Márquez?
-    Should we refer to the author of "Tom Sawyer" as Samuel Clemens or
-    Mark Twain? Are Voltaire and Molière first names or last names? etc.
-
-    A book is represented as a dictionary with the keys 'id', 'title',
-    and 'publication_year'. For example, "Pride and Prejudice"
-    (assuming an ID of 132) would look like this:
-
-        {'id': 193, 'title': 'A Wild Sheep Chase', 'publication_year': 1982}
-
-    '''
 
     def __init__(self, books_filename, authors_filename, books_authors_link_filename):
-        ''' Initializes this data source from the three specified  CSV files, whose
-            CSV fields are:
-
-                books: ID,title,publication-year
-                  e.g. 6,Good Omens,1990
-                       41,Middlemarch,1871
-
-
-                authors: ID,last-name,first-name,birth-year,death-year
-                  e.g. 5,Gaiman,Neil,1960,NULL
-                       6,Pratchett,Terry,1948,2015
-                       22,Eliot,George,1819,1880
-
-                link between books and authors: book_id,author_id
-                  e.g. 41,22
-                       6,5
-                       6,6
-
-                  [that is, book 41 was written by author 22, while book 6
-                    was written by both author 5 and author 6]
-
-            Note that NULL is used to represent a non-existent (or rather, future and
-            unknown) year in the cases of living authors.
-
-            NOTE TO STUDENTS: I have not specified how you will store the books/authors
-            data in a BooksDataSource object. That will be up to you, in Phase 3.
-        '''
-
         self.books = books_filename
         self.authors = authors_filename
         self.books_authors = books_authors_link_filename
+
+    def openfile(self, filename):
+         with open(filename, newline = "") as csvfile:
+             list = csv.reader(csvfile)
+             sorted_list = sorted(list, key=operator.itemgetter(0))
+             return sorted_list
 
     def book(self, book_id):
         ''' Returns the book with the specified ID. (See the BooksDataSource comment
@@ -90,12 +32,10 @@ class BooksDataSource:
         if book_id > 46 or book_id < 0:
             raise ValueError("what")
         else:
-            with open(self.books, newline = "") as csvfile:
-                list = csv.reader(csvfile)
-                sorted_list = sorted(list, key=operator.itemgetter(0))
-                for book in sorted_list:
-                    if book[0] == str(book_id):
-                        return book[1]
+            sorted_list = openfile(self.books)
+            for book in sorted_list:
+                if book[0] == str(book_id):
+                    return book[1]
 
     def books(self, *, author_id=None, search_text=None, start_year=None, end_year=None, sort_by='title'):
         ''' Returns a list of all the books in this data source matching all of
@@ -162,3 +102,4 @@ class BooksDataSource:
             See the BooksDataSource comment for a description of how an author is represented.
         '''
         return []
+
