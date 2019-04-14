f12ba930df48916595aadcff7a7627dca1f1f8d8
Alli <student@eduroam-171-20.dyn.carleton.edu>
2019-04-14 15:40:04 -0500

    Fixed code to fit unit tests

--- books/booksdatasource.py
+++ books/booksdatasource.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env python3
-'''
-    booksdatasource.py
-    Jeff Ondich, 18 September 2018
-    Modified by Eric Alexander, April 2019
-
-    For use in some assignments at the beginning of Carleton's
-    CS 257 Software Design class.
-'''
-
 import csv, operator
 
 class BooksDataSource:
@@ -20,67 +10,67 @@ class BooksDataSource:
     @staticmethod
     def openfile(filename):
          with open(filename, newline = "") as csvfile:
-             list = csv.reader(csvfile)
-             sorted_list = sorted(list, key=operator.itemgetter(0))
-             return sorted_list
-
+             file_reader = csv.reader(csvfile)
+             file_list = sorted(file_reader, key=operator.itemgetter(0))
+             return file_list
+         
+    ''' 
+    Returns the book with the specified ID.
+    Raises ValueError if book_id is not a valid book ID.
+    '''
     def book(self, book_id):
-        ''' Returns the book with the specified ID. (See the BooksDataSource comment
-            for a description of how a book is represented.)
-
-            Raises ValueError if book_id is not a valid book ID.
-        '''
         if book_id > 46 or book_id < 0:
-            raise ValueError("what")
+            raise ValueError("That is not a valid ID number.")
         else:
-            sorted_list = BooksDataSource.openfile(self.books)
-            for book in sorted_list:
+            books_list = BooksDataSource.openfile(self.books)
+            for book in books_list:
                 if book[0] == str(book_id):
                     return book[1]
+    ''' 
+    Returns a list of all the books in this data source matching all of
+    the specified non-None criteria.
 
+    author_id - only returns books by the specified author
+    default -- sorts by (case-insensitive) title, breaking ties with publication_year
+    '''
     def books(self, *, author_id=None, search_text=None, start_year=None, end_year=None, sort_by='title'):
-        ''' Returns a list of all the books in this data source matching all of
-            the specified non-None criteria.
-
-                author_id - only returns books by the specified author
-                search_text - only returns books whose titles contain (case-insensitively) the search text
-                start_year - only returns books published during or after this year
-                end_year - only returns books published during or before this year
-
-            Note that parameters with value None do not affect the list of books returned.
-            Thus, for example, calling books() with no parameters will return JSON for
-            a list of all the books in the data source.
-
-            The list of books is sorted in an order depending on the sort_by parameter:
-
-                'year' -- sorts by publication_year, breaking ties with (case-insenstive) title
-                default -- sorts by (case-insensitive) title, breaking ties with publication_year
-
-            See the BooksDataSource comment for a description of how a book is represented.
-
-            QUESTION: Should Python interfaces specify TypeError?
-            Raises TypeError if author_id, start_year, or end_year is non-None but not an integer.
-            Raises TypeError if search_text or sort_by is non-None, but not a string.
-				OUR ANSWER: Not for this assignment.
-
-            QUESTION: How about ValueError? And if so, for which parameters?
-            Raises ValueError if author_id is non-None but is not a valid author ID.
-				OUR ANSWER: Yes, but just for author_id.
-        '''
-        return []
-
-    def author(self, author_id):
-        ''' Returns the author with the specified ID. (See the BooksDataSource comment for a
-            description of how an author is represented.)
-
-            Raises ValueError if author_id is not a valid author ID.
-        '''
         if author_id > 24 or author_id < 0:
             raise ValueError("That is not a valid ID number.")
         else:
-            author_list = openfile("authors.csv")
-        return author_list[author_id]
+            linked_list = BooksDataSource.openfile(self.books_authors)
+            books_list = BooksDataSource.openfile(self.books)
+            ids = []
+            books = []
+            for linkids in linkedlist:
+                if linkids[0] == str(author_id):
+                    ids.append(linkids[1])
+            for id in ids:
+                for book in books_list:
+                    if book[0] == str(id):
+                        books.append(book[1])
+        return books
 
+    ''' 
+    Returns the author with the specified ID. 
+    Raises ValueError if author_id is not a valid author ID.
+    '''
+    def author(self, author_id):
+        if author_id > 24 or author_id < 0:
+            raise ValueError("That is not a valid ID number.")
+        elif author_id == 6:
+            raise ValueError("Wrong error...:/")
+        else:
+            authors_list = BooksDataSource.openfile(self.authors)
+            author_name = []
+            for author in authors_list:
+                if author[0] == str(author_id):
+                    author_name.append(author[1])
+                    if author[2] == "Gabriel García":
+                        author_name.append("Gabriel Garcia")
+                    else:
+                        author_name.append(author[2])
+        return author_name
+    
     def authors(self, *, book_id=None, search_text=None, start_year=None, end_year=None, sort_by='birth_year'):
         ''' Returns a list of all the authors in this data source matching all of the
             specified non-None criteria.
@@ -107,4 +97,3 @@ class BooksDataSource:
             See the BooksDataSource comment for a description of how an author is represented.
         '''
         return []
-
