0115a22e60ca5aeb4ac5320efa47e317237d6817
Alli <student@eduroam-171-20.dyn.carleton.edu>
2019-04-15 11:35:12 -0500

    Added code for books

--- books/booksdatasource.py
+++ books/booksdatasource.py
@@ -37,22 +37,19 @@ class BooksDataSource:
         if author_id > 24 or author_id < 0:
             raise ValueError("That is not a valid ID number.")
         else:
-            '''
             linked_list = BooksDataSource.openfile(self.books_authors)
-            books_list = BooksDataSource.openfile(self.books)
+            books_list = BooksDataSource.openfile(self.bookss)
             ids = []
             books = []
-            for linkids in linkedlist:
-                if linkids[0] == str(author_id):
-                    ids.append(linkids[1])
+            for linkids in linked_list:
+                if linkids[1] == str(author_id):
+                    ids.append(linkids[0])
             for id in ids:
                 for book in books_list:
                     if book[0] == str(id):
                         books.append(book[1])
-        return books
-        '''
-            return []
-
+            return books
+        
     ''' 
     Returns the author with the specified ID. 
     Raises ValueError if author_id is not a valid author ID.
@@ -70,34 +67,15 @@ class BooksDataSource:
                         author_name.append("Gabriel Garcia")
                     else:
                         author_name.append(author[2])
-        return author_name
+            return author_name
     
     def authors(self, *, book_id=None, search_text=None, start_year=None, end_year=None, sort_by='birth_year'):
-        ''' Returns a list of all the authors in this data source matching all of the
-            specified non-None criteria.
-
-                book_id - only returns authors of the specified book
-                search_text - only returns authors whose first or last names contain
-                    (case-insensitively) the search text
-                start_year - only returns authors who were alive during or after
-                    the specified year
-                end_year - only returns authors who were alive during or before
-                    the specified year
-
-            Note that parameters with value None do not affect the list of authors returned.
-            Thus, for example, calling authors() with no parameters will return JSON for
-            a list of all the authors in the data source.
-
-            The list of authors is sorted in an order depending on the sort_by parameter:
-
-                'birth_year' - sorts by birth_year, breaking ties with (case-insenstive) last_name,
-                    then (case-insensitive) first_name
-                any other value - sorts by (case-insensitive) last_name, breaking ties with
-                    (case-insensitive) first_name, then birth_year
-
-            See the BooksDataSource comment for a description of how an author is represented.
-        '''
         if book_id != None:
             if book_id > 48 or book_id < 0:
                 raise ValueError("That is not a valid ID number.")
-        return []
+            else:
+
+                
+        else:
+            
+        
